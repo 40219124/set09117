beb5e20ff06f7b2075cb4da54c3a82e08f161b52
Tim-Surface <40219124@live.napier.ac.uk>
2017-11-13 17:48:32 +0000

    Finished deque class.

--- Draughts/Deque.py
+++ Draughts/Deque.py
@@ -2,3 +2,24 @@ class Deque(object):
 
     def __init__(self):
         self.deque = []
+
+    def add_rear(self, move):
+        self.deque.insert(0, move)
+
+    def remove_rear(self):
+        return self.deque.pop(0)
+
+    def add_front(self, move):
+        self.deque.append(move)
+
+    def remove_front(self):
+        return self.deque.pop()
+
+    def is_empty(self):
+        return self.size() == 0
+
+    def size(self):
+        return len(self.deque)
+
+    def clear(self):
+        self.deque = []
